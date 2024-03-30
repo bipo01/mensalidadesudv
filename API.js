7f28fa5cc3b9f4a39336df950282c435d49298a5
João Bispo <jsbispo1101@gmail.com>
2024-03-30 00:52:58 -0400

    senha correta

--- API.js
+++ API.js
@@ -15,18 +15,19 @@ db.connect();
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    if (req.query.senha === process.env.SENHA_ADM) {
         const result = await db.query(
             "SELECT * FROM mensalidadesudv ORDER BY nomesocio DESC"
         );
         const data = result.rows;
 
         res.json(data);
+        123;
     }
 });
 
 app.get("/filter", async (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    if (req.query.senha === process.env.SENHA_ADM) {
         const situacaoQuery = req.query.situacao;
         const socioQuery = req.query.nomesocio;
         const grauQuery = req.query.grau;
@@ -40,14 +41,15 @@ app.get("/filter", async (req, res) => {
         const textoDb = [];
 
         for (let [key, value] of entries) {
-            if (value === "Todos") {
-                value = "''";
-            }
-            if (value.trim() !== "''") {
-                textoDb.push(`${key} = '${value}'`);
+            if (key !== "senha") {
+                if (value === "Todos") {
+                    value = "''";
+                }
+                if (value.trim() !== "''") {
+                    textoDb.push(`${key} = '${value}'`);
+                }
             }
         }
-
         let textoDbPronto = `${
             textoDb.length > 0 ? "WHERE" : ""
         } ${textoDb.join(" AND ")}`;
@@ -65,7 +67,7 @@ app.get("/filter", async (req, res) => {
 });
 
 app.get("/novo", async (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    if (req.query.senha === process.env.SENHA_ADM) {
         db.query(
             "INSERT INTO mensalidadesudv (nomesocio, grau) VALUES ($1, $2)",
             [req.query.nomesocio, req.query.lugar]
@@ -76,7 +78,7 @@ app.get("/novo", async (req, res) => {
 });
 
 app.get("/salvar", async (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    if (req.query.senha === process.env.SENHA_ADM) {
         console.log(req.query.condicao);
         console.log(req.query.mes);
         console.log(req.query.nomesocio);
@@ -96,7 +98,8 @@ app.get("/salvar", async (req, res) => {
 });
 
 app.get("/situation", async (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    console.log(req.query.senha);
+    if (req.query.senha === process.env.SENHA_ADM) {
         console.log(req.query.situacao);
         db.query(
             `UPDATE mensalidadesudv SET situacao = $1 WHERE nomesocio = $2`,
@@ -108,15 +111,18 @@ app.get("/situation", async (req, res) => {
     }
 });
 
-app.get("/excluir", (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+app.get("/excluir", async (req, res) => {
+    if (req.query.senha === process.env.SENHA_ADM) {
         db.query("DELETE FROM mensalidadesudv WHERE id = $1", [req.query.id]);
-        res.json("Sócio Eliminado");
+
+        const result = await db.query("SELECT * FROM mensalidadesudv");
+        const data = result.rows;
+        res.json(data);
     }
 });
 
 app.get("/alternar", (req, res) => {
-    if (req.params.senha === process.env.SENHA_ADM) {
+    if (req.query.senha === process.env.SENHA_ADM) {
         db.query(
             `UPDATE mensalidadesudv SET ${req.query.mes} = $1 WHERE id = $2`,
             [req.query.condicao, req.query.id]
