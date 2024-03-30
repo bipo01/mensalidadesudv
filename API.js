13d47827ba3d96186d8bbc11f734ab8c3a229851
João Bispo <jsbispo1101@gmail.com>
2024-03-30 00:09:54 -0400

    senha_adm

--- API.js
+++ API.js
@@ -15,101 +15,115 @@ db.connect();
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    const result = await db.query(
-        "SELECT * FROM mensalidadesudv ORDER BY nomesocio DESC"
-    );
-    const data = result.rows;
+    if (req.params.senha === process.env.SENHA_ADM) {
+        const result = await db.query(
+            "SELECT * FROM mensalidadesudv ORDER BY nomesocio DESC"
+        );
+        const data = result.rows;
 
-    res.json(data);
+        res.json(data);
+    }
 });
 
 app.get("/filter", async (req, res) => {
-    const situacaoQuery = req.query.situacao;
-    const socioQuery = req.query.nomesocio;
-    const grauQuery = req.query.grau;
+    if (req.params.senha === process.env.SENHA_ADM) {
+        const situacaoQuery = req.query.situacao;
+        const socioQuery = req.query.nomesocio;
+        const grauQuery = req.query.grau;
 
-    const queries = req.query;
-    console.log(queries);
+        const queries = req.query;
+        console.log(queries);
 
-    const entries = Object.entries(queries);
-    console.log(entries);
+        const entries = Object.entries(queries);
+        console.log(entries);
 
-    const textoDb = [];
+        const textoDb = [];
 
-    for (let [key, value] of entries) {
-        if (value === "Todos") {
-            value = "''";
+        for (let [key, value] of entries) {
+            if (value === "Todos") {
+                value = "''";
+            }
+            if (value.trim() !== "''") {
+                textoDb.push(`${key} = '${value}'`);
+            }
         }
-        if (value.trim() !== "''") {
-            textoDb.push(`${key} = '${value}'`);
-        }
+
+        let textoDbPronto = `${
+            textoDb.length > 0 ? "WHERE" : ""
+        } ${textoDb.join(" AND ")}`;
+        console.log(textoDbPronto);
+
+        const result = await db.query(
+            `SELECT * FROM mensalidadesudv ${textoDbPronto} ORDER BY nomesocio DESC`
+        );
+        const data = result.rows;
+
+        console.log(data);
+
+        res.json(data);
     }
-
-    let textoDbPronto = `${textoDb.length > 0 ? "WHERE" : ""} ${textoDb.join(
-        " AND "
-    )}`;
-    console.log(textoDbPronto);
-
-    const result = await db.query(
-        `SELECT * FROM mensalidadesudv ${textoDbPronto} ORDER BY nomesocio DESC`
-    );
-    const data = result.rows;
-
-    console.log(data);
-
-    res.json(data);
 });
 
 app.get("/novo", async (req, res) => {
-    db.query("INSERT INTO mensalidadesudv (nomesocio, grau) VALUES ($1, $2)", [
-        req.query.nomesocio,
-        req.query.lugar,
-    ]);
+    if (req.params.senha === process.env.SENHA_ADM) {
+        db.query(
+            "INSERT INTO mensalidadesudv (nomesocio, grau) VALUES ($1, $2)",
+            [req.query.nomesocio, req.query.lugar]
+        );
 
-    res.json("Adicionado");
+        res.json("Adicionado");
+    }
 });
 
 app.get("/salvar", async (req, res) => {
-    console.log(req.query.condicao);
-    console.log(req.query.mes);
-    console.log(req.query.nomesocio);
-    if (req.query.nomesocio === "Todos") {
-        db.query(`UPDATE mensalidadesudv SET ${req.query.mes} = $1`, [
-            req.query.condicao,
-        ]);
-    } else {
-        db.query(
-            `UPDATE mensalidadesudv SET ${req.query.mes} = $1 WHERE nomesocio = '${req.query.nomesocio}'`,
-            [req.query.condicao]
-        );
-    }
+    if (req.params.senha === process.env.SENHA_ADM) {
+        console.log(req.query.condicao);
+        console.log(req.query.mes);
+        console.log(req.query.nomesocio);
+        if (req.query.nomesocio === "Todos") {
+            db.query(`UPDATE mensalidadesudv SET ${req.query.mes} = $1`, [
+                req.query.condicao,
+            ]);
+        } else {
+            db.query(
+                `UPDATE mensalidadesudv SET ${req.query.mes} = $1 WHERE nomesocio = '${req.query.nomesocio}'`,
+                [req.query.condicao]
+            );
+        }
 
-    res.json("Atualizado");
+        res.json("Atualizado");
+    }
 });
 
 app.get("/situation", async (req, res) => {
-    console.log(req.query.situacao);
-    db.query(`UPDATE mensalidadesudv SET situacao = $1 WHERE nomesocio = $2`, [
-        req.query.situacao,
-        req.query.nomesocio,
-    ]);
-    console.log("Deu certo");
+    if (req.params.senha === process.env.SENHA_ADM) {
+        console.log(req.query.situacao);
+        db.query(
+            `UPDATE mensalidadesudv SET situacao = $1 WHERE nomesocio = $2`,
+            [req.query.situacao, req.query.nomesocio]
+        );
+        console.log("Deu certo");
 
-    res.json("Situação Alterada");
+        res.json("Situação Alterada");
+    }
 });
 
 app.get("/excluir", (req, res) => {
-    db.query("DELETE FROM mensalidadesudv WHERE id = $1", [req.query.id]);
-    res.json("Sócio Eliminado");
+    if (req.params.senha === process.env.SENHA_ADM) {
+        db.query("DELETE FROM mensalidadesudv WHERE id = $1", [req.query.id]);
+        res.json("Sócio Eliminado");
+    }
 });
 
 app.get("/alternar", (req, res) => {
-    db.query(`UPDATE mensalidadesudv SET ${req.query.mes} = $1 WHERE id = $2`, [
-        req.query.condicao,
-        req.query.id,
-    ]);
+    if (req.params.senha === process.env.SENHA_ADM) {
+        db.query(
+            `UPDATE mensalidadesudv SET ${req.query.mes} = $1 WHERE id = $2`,
+            [req.query.condicao, req.query.id]
+        );
 
-    res.json("Alterado");
+        res.json("Alterado");
+    }
 });
 
 app.listen(port, () => {
